d093a970a1242be786bfad070c71b2123d3b2bd5
StefaniPencheva <s.pencheva2018@pmg-pernik.com>
2022-02-08 13:18:53 +0200

    Arrays

--- ZadArrays/Zad2/Program.cs
+++ ZadArrays/Zad2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Zad2
 {
@@ -6,7 +7,41 @@ namespace Zad2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var arr = Console.ReadLine().Split().ToArray();
+            while (true)
+            {
+                var cmd = Console.ReadLine().Split().ToArray();
+                string command = cmd[0];
+                if (command=="END")
+                {
+                    break;
+                }
+                switch (command)
+                {
+                    case "Reverse":
+                        arr = arr.Reverse().ToArray();
+                        break;
+                    case "Distinct":
+                        arr = arr.Distinct().ToArray();
+                        break;
+                    case "Replace":
+                        int index = int.Parse(cmd[1]);
+                        string newWord = cmd[2];
+                        if (index>=0 && index<arr.Length)
+                        {
+                            arr[index] = newWord;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input!");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command!");
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(", ", arr));
         }
     }
 }
