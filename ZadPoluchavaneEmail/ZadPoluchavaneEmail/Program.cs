33739269cde975447a961c146944d74420bce5ba
StefaniPencheva <s.pencheva2018@pmg-pernik.com>
2022-03-01 12:59:59 +0200

    zadtest

---
+++ ZadPoluchavaneEmail/ZadPoluchavaneEmail/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZadPoluchavaneEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> emails = new Dictionary<string, int>();
+            while (true)
+            {
+                var command = Console.ReadLine().Split();
+                if (command[0] == "List")
+                {
+                    break;
+                }
+                if (command[0] == "Add")
+                {
+                    var name = command[1];
+                    var email = command[2];
+                    if (!emails.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{emails} already exists.");
+                    }
+                    else
+                    {
+                        emails.Add(name, email);
+                    }
+                }
+                if (command[0] == "Receive")
+                {
+                    var letters = input[1];
+                    if (!emails.ContainsKey(letters && letters.Contains))
+                }
+            }
+        }
+    }
+}
