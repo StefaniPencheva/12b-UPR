c491e573d852264525e1d843e04f349cdce09a8c
StefaniPencheva <s.pencheva2018@pmg-pernik.com>
2022-03-07 10:50:48 +0200

    Emails

---
+++ Toys/Toys/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Toys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> toys = new Dictionary<string, int>();
+            while (true)
+            {
+                var command = Console.ReadLine().Split();
+                if (command[0] == "Stop")
+                {
+                    foreach (var item in toys)
+                    {
+                        string toy = item.Key;
+                        int q = item.Value;
+                        Console.WriteLine($"{toy} -> {q}");
+                    }
+                    break;
+                }
+                switch (command[0])
+                {
+                    case "Purchase":
+                        {
+                            string toy = command[1];
+                            int count = int.Parse(command[2]);
+
+                            if (toy.StartsWith("d"))
+                            {
+                                if (!toys.ContainsKey(toy))
+                                {
+                                    toys.Add(toy, count);
+                                }
+                                else
+                                {
+                                    toys[toy] += count;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Input is not correct!");
+                            }
+                            break;
+                        }
+                    case "Sale":
+                        {
+                            string toy = command[1];
+
+                            if (!toys.ContainsKey(toy))
+                            {
+                                Console.WriteLine($"{toy} does not exists!");
+                            }
+                            else
+                            {
+                                if (toys[toy] > 0)
+                                {
+                                    toys[toy]--;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Sale is not allowed!");
+                                }
+                            }
+                            break;
+                        }
+                    case "All sale":
+                        {
+                            foreach (var item in toys)
+                            {
+                                toys[item.Key] = 0;
+                            }
+                            break;
+                        }
+                    default:
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ Wardrobe/Wardrobe/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> clothesColor = new Dictionary<string, string>();
+            Dictionary<string, int> clothesCount = new Dictionary<string, int>();
+
+            while (true)
+            {
+                var input = Console.ReadLine().Split(); 
+
+                if (input[0] == "Show")
+                {
+                    break;
+                }
+
+                switch (input[0])
+                {
+                    case "AddColor":
+                        {
+                            string clothe = input[1];
+                            string color = input[2];
+                            if (clothe.StartsWith("t"))
+                            {
+                                if (!clothesColor.ContainsKey(clothe))
+                                {
+                                    clothesColor.Add(clothe, color);
+                                    clothesCount.Add(clothe, 1);
+                                }
+                                else
+                                {
+                                    clothesColor[clothe] = color;
+                                    clothesCount[clothe] += 1;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid input!");
+                            }
+                            break;
+                        }
+                    case "AddQuantity":
+                        {
+                            string clothe = input[1];
+                            int count = int.Parse(input[2]); 
+                            if (clothesColor.ContainsKey(clothe))
+                            {
+                                clothesCount[clothe] += count;
+                            }
+                            break;
+                        }
+                    case "Remove":
+                        {
+                            string clothe = input[1];
+                            int count = int.Parse(input[2]);
+
+                            if (clothesCount.ContainsKey(clothe))
+                            {
+                                if (count >= clothesCount[clothe])
+                                {
+                                    Console.WriteLine("Not enough quantity!");
+                                }
+                                else
+                                {
+                                    clothesCount[clothe] -= count;
+                                }
+                            }
+                            break;
+                        }
+                    case "Empty":
+                        {
+                            foreach (var clothe in clothesCount)
+                            {
+                                clothesCount[clothe.Key] = 0;
+                            }
+                            foreach (var clothe in clothesColor)
+                            {
+                                clothesColor[clothe.Key] = "blank";
+                            }
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+            int grandTotal = 0;
+
+            foreach (var clothe in clothesCount)
+            {
+                string clotherName=clothe.Key;
+                int clotheCount = clothe.Value;
+                string clotheColor = clothesColor[clotherName];
+
+                Console.WriteLine($"{clotherName}: {clotheColor} - {clotheCount}");
+
+                grandTotal += clotheCount;
+            }
+            Console.WriteLine($"Grand Total: {grandTotal}");
+
+        }
+    }
+}
