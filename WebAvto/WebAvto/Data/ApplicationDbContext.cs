e12dff64053ff8680a0339b9476efa8381a700dd
StefaniPencheva <s.pencheva2018@pmg-pernik.com>
2023-01-13 11:16:58 +0200

    WebAvto

---
+++ WebAvto/WebAvto/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebAvto.Entities;
+
+namespace WebAvto.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+
+    }
+}
---
+++ WebAvto/WebAvto/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAvto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
