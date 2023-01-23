9bf7aafe9637142a5f895b4ead93ef1d4414b493
StefaniPencheva <s.pencheva2018@pmg-pernik.com>
2023-01-23 13:33:33 +0200

    Dogs

---
+++ ZadDogsIzlojba/ZadDogsIzlojba/Controllers/DogController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZadDogsIzlojba.Data;
+using ZadDogsIzlojba.Domain;
+using ZadDogsIzlojba.Models;
+
+namespace ZadDogsIzlojba.Controllers
+{
+    public class DogController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        
+        public DogController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(DogCreateViewModel bindingModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Dog dogFormDb = new Dog
+                {
+                    Name = bindingModel.Name,
+                    Age = bindingModel.Age,
+                    Breed = bindingModel.Breed,
+                    Picture = bindingModel.Picture,
+                };
+            context.Dogs.Add(dogFormDb);
+            context.SaveChanges();
+
+                return this.RedirectToAction("Success");
+            }
+            return this.View();
+        }
+
+        public IActionResult Success()
+        {
+            return this.View();
+        }
+
+        public IActionResult All()
+        {
+            
+            List<DogAllViewModel> dogs = context.Dogs
+                .Select(dogFromDb => new DogAllViewModel
+                {
+                    Id=dogFromDb.Id,
+                    Name = dogFromDb.Name,
+                    Age = dogFromDb.Age,
+                    Breed = dogFromDb.Breed,
+                    Picture = dogFromDb.Picture
+                }
+            ).ToList();
+            return this.View(dogs);
+        }
+    }
+}
+       
---
+++ ZadDogsIzlojba/ZadDogsIzlojba/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ZadDogsIzlojba.Domain;
+
+namespace ZadDogsIzlojba.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Dog> Dogs { get; set; }
+        
+    }
+}
